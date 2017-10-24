9748f5f6a70491842ab6456ff4f0492f3d817d1e
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-10-24 14:03:42 -0400

    testing

--- Game/DDR Prototype/Assets/Graphics/arrowDown.cs
+++ Game/DDR Prototype/Assets/Graphics/arrowDown.cs
@@ -20,7 +20,7 @@ public class arrowDown : MonoBehaviour {
 		float tracker=10f;
 //		float newPosition; 
 
-		transform.Translate (Vector3.right * 2 * Time.deltaTime); 
+		transform.Translate (Vector3.right * 3 * Time.deltaTime); 
 		System.Threading.Thread.Sleep(100);
 		while (tracker >= 0) {
 			transform.Translate (Vector3.down * dropSpeed * Time.deltaTime); 
