f0ed783d92268080982174c579dc3eae40a9cbf8
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-10-24 13:22:01 -0400

    mr goose help us

---
+++ Game/DDR Prototype/Assets/Graphics/arrowLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowLeft : MonoBehaviour {
+
+	public float dropSpeed = 1f; 
+	public GameObject theLeftArrow;  
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		float tracker = 10f;
+
+		transform.Translate (Vector3.right * 1 * Time.deltaTime); 
+		System.Threading.Thread.Sleep(100);
+
+		while (tracker > 0) {
+			transform.Translate (Vector3.down * dropSpeed * Time.deltaTime); 
+			tracker -= 1; 
+			Vector3 theLeftArrow = transform.position;
+
+			if (transform.position.y == 0) {
+				break; 
+			}
+		}
+	}
+}
---
+++ Game/DDR Prototype/Assets/Graphics/arrowRight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowRight : MonoBehaviour {
+
+	public float dropSpeed = 1f; 
+	public GameObject theRightArrow;  
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		float tracker = 10f;
+
+	
+
+		transform.Translate (Vector3.left * 2 * Time.deltaTime); 
+		System.Threading.Thread.Sleep(100);
+
+
+		while (tracker > 0) {
+			transform.Translate (Vector3.down * dropSpeed * Time.deltaTime); 
+			tracker -= 1; 
+			Vector3 theRightArrow = transform.position;
+
+			if (transform.position.y == 0) {
+				break; 
+			}
+		}
+	}
+}
