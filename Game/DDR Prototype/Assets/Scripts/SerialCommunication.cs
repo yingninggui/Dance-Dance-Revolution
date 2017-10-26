1f14336f37990bcb0542becaf2077e63f0f0bb99
Bimesh De Silva <b2desilv@edu.uwaterloo.ca>
2017-10-25 21:30:29 -0400

    Moved serial communication to seperate thread to improve performance on main thread

--- Game/DDR Prototype/Assets/Scripts/SerialCommunication.cs
+++ Game/DDR Prototype/Assets/Scripts/SerialCommunication.cs
@@ -1,28 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System;
+using System.Threading;
 using System.IO.Ports;
 
 public class SerialCommunication : MonoBehaviour {
 
     public GameObject GameScreen;
+    bool running;
+    Thread thread;
     private const string PORT = "\\\\.\\COM8";
     private SerialPort serial = new SerialPort(PORT, 9600);
 
     // Use this for initialization
     void Start () {
+        thread = new Thread(StartSerialCommunication);
+        thread.Start();
+    }
+
+    void StartSerialCommunication()
+    {
         Debug.Log("Launched Start Method");
 
         Debug.Log(SerialPort.GetPortNames());
-
-        serial.Open();
+        running = true;
+        try
+        {
+            serial.Open();
+        } catch (Exception e) {
+            Debug.LogError(e.Message);
+        }
 
         Debug.Log("Initialized Port " + PORT);
+
+        while (running)
+        {
+            try
+            {
+                Debug.Log(serial.ReadLine());
+            } catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void OnDisable()
+    {
+        Debug.Log("Exiting Thread");
+        if (running)
+        {
+            running = false;
+            thread.Join();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
         /*
         try {
             string message = serial.ReadLine();
