bc4aefb811f545b21268fad2ac54a7822a8f1de9
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-11-20 20:20:36 -0500

    Constructing arrow motion

--- Game/DDR Prototype/Assets/Scripts/arrowDown.cs
+++ Game/DDR Prototype/Assets/Scripts/arrowDown.cs
@@ -1,39 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 //
-//public Point Location { get; set; }
-
-public class arrowDown : MonoBehaviour {
-
-	public float dropSpeed = 1f; 
-	public GameObject theDownArrow;  
-
-	void Start () {
-		
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		float tracker=1f;
-
-
-//		transform.Translate (Vector3.right * 3 * Time.deltaTime);
-
-		while (tracker >= 0) {
-			transform.Translate (Vector3.down * dropSpeed * Time.deltaTime); 
-			tracker -= 1; 
-			Vector3 theDownArrow = transform.position;
-
-			if (transform.position.y == 0) {
-				break; 
-			}
-
-		}
-					
-	}
-
-
-}
+////
+////public Point Location { get; set; }
+//
+//public class arrowDown : MonoBehaviour {
+//
+//	public float dropSpeed = 1f; 
+//	public GameObject theDownArrow;  
+//
+//	void Start () {
+//		
+//	}
+//
+//	// Update is called once per frame
+//	void Update () {
+//
+//		float tracker=1f;
+//
+//
+////		transform.Translate (Vector3.right * 3 * Time.deltaTime);
+//
+//		while (tracker >= 0) {
+//			transform.Translate (Vector3.down * dropSpeed * Time.deltaTime); 
+//			tracker -= 1; 
+//			Vector3 theDownArrow = transform.position;
+//
+//			if (transform.position.y == 0) {
+//				break; 
+//			}
+//
+//		}
+//					
+//	}
+//
+//
+//}
--- Game/DDR Prototype/Assets/Scripts/arrows.cs
+++ Game/DDR Prototype/Assets/Scripts/arrows.cs
@@ -2,15 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class createArrows: MonoBehaviour {
+
+	HashSet<GameObject> arrowActive = new HashSet<GameObject> (); 
+	Queue<GameObject> arrowInactive = new Queue<GameObject> (); 
+
+	public int arrowsCount = 100; 
+	public GameObject arrowPrefab; 
+//
+//	public GameObject leftArrow;  
+//	public GameObject rightArrow;
+//	public GameObject upArrow; 
+//	public GameObject downArrow; 
+//
+	public const int left = 1;
+	public const int right = 2;
+	public const int up = 3;
+	public const int down = 4; 
+
+
+	void Start () {
+
+		GameObject arrow = Instantiate (arrowPrefab); 
+
+		for (int i = 0; i < arrowsCount; i++) {
+			arrowInactive.Enqueue (createGameObject ()); 
+		}
+	}
+
+	GameObject createGameObject(int direction){
+		GameObject arrow = Instantiate (arrowPrefab); 
+		arrow.transform.GetComponentInParent = transform; 
+		arrow.gameObject.SetActive(false);
+
+		switch(direction) {
+			case left: 
+				arrow = transform.Rotate (270); 
+				break; 
+
+			case right: // right
+				arrow = transform.Rotate(90); 
+				break; 
+
+			case up:
+				arrow = transform.Rotate(0); 
+				break; 
+
+			case down:
+				transform.Rotate(180); 
+				break;
+
+		}
+
+		return arrow;  
+	}
+
+
+}
+
 public class arrows : MonoBehaviour {
+
+	public float dropSpeed = 1f; 
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float tracker = 1f; 
+
+		while (tracker > 0) {
+			transform.Translate (Vector3.down * dropSpeed * Time.deltaTime); 
+			tracker -= 1; 
+			Vector3 leftArrow = transform.position;
+			Vector3 rightArrow = transform.position;
+			Vector3 upArrow = transform.position;
+			Vector3 downArrow = transform.position;
+
+			if (transform.position.y == 0) {
+				break; 
+			}
+		}
+	}
+
+	void generateArrows() {
+		
 		
 	}
+
 }
