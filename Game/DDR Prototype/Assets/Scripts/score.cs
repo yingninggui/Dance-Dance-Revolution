2d44e98cfc308cf6be30df0f5d8df2018495554d
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-11-20 23:39:25 -0500

    add text to screen via GameObject

--- Game/DDR Prototype/Assets/Scripts/score.cs
+++ Game/DDR Prototype/Assets/Scripts/score.cs
@@ -3,33 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class score : MonoBehaviour {
-	public GameObject scoreKeeper; 
-	public int hits; 
-	public int misses; 
+	public GameObject scoreKeeper = new GameObject("Score"); 
+	public string hits; 
+	public string misses; 
 
 	void Start () {
-		hits = 0;
-		misses = 0; 
-		SetScoreText ();
-		hits.text = "";
 	}
 
 	void Update (){
+		Text text = scoreKeeper.GetComponent<Text> ();
+		text.text = hits;
+	}
 		
+	void PrintResults (string hitsNumber, string missesNumber){
+		Text text = scoreKeeper.GetComponent<Text> ();
+		hits = ""; 
+		hits += hitsNumber; 
+		text.text = hits; 
+		misses = ""; 
+		misses += missesNumber; 
+		text.text = misses; 
+		scoreKeeper.AddComponent<Text> ();
 	}
-
-	void OnTriggerEnter(Collider arrowTapped) {
-		if (arrowTapped) {
-			SetscoreCountText (); 
-			scoreCount++; 
-		} else {
-			
-		}
-	}
-	void SetScoreText (){
-		hits.Text = hits.ToString ();
-	}
-
-}
+}	
 
