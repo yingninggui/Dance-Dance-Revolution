c06373a399a894551c0f9513cb1033d40e946f44
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-11-21 18:49:29 -0500

    apaprently unity decided to make the loadlevel document obsolete so this scene mess was converted to scene management hmm . ok

--- Game/DDR Prototype/Assets/Scripts/GameNavigation.cs
+++ Game/DDR Prototype/Assets/Scripts/GameNavigation.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 
 public class GameNavigation : MonoBehaviour {
 
@@ -8,26 +10,63 @@ public class GameNavigation : MonoBehaviour {
 	public GameObject gameSummarySelected; 
 	public GameObject startSelected; 
 
-	public void LoadScene(int scene) {
+	public Texture play; 
 
-		if (scene == 1) {
-			musicSelected.SetActive (true); 
-			gameSummarySelected.SetActive (false); 
-			startSelected.SetActive (false);
-		} else if (scene == 2) {
-			gameSummarySelected.SetActive (true); 
-			musicSelected.SetActive (false); 
-			startSelected.SetActive (false);
-		}
-		else {
-			startSelected.SetActive (true);
-			gameSummarySelected.SetActive (false); 
-			musicSelected.SetActive (false); 
-		}
+	public class ExampleClass : MonoBehaviour {
+		public Texture btnTexture;
+		void OnGUI() {
+			if (!btnTexture) {
+				Debug.LogError("Please assign a texture on the inspector");
+				return;
+			}
+			if (GUI.Button(new Rect(10, 10, 50, 50), btnTexture))
+				Debug.Log("Clicked the button with an image");
 
-		Application.LoadLevel(scene);
+			if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+				Debug.Log("Clicked the button with text");
+
+		}
 	}
 
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 10, 5, 5), "Play")) {
+			SceneManager.LoadScene ("GameScreen");
+		}
+		if (GUI.Button (new Rect (10, 10, 5, 5), "Play Again")) {
+			SceneManager.LoadScene  ("Start");
+		}
+		if (gameEnd(1)) {
+			SceneManager.LoadScene ("GameSummary");
+		}
+	}
+	bool gameEnd(int end) {
+		if (end == 1) {
+			return true; 
+		} else {
+			return false; 
+		}
+	}
+
+//	public void LoadScene(int scene) {
+//
+//		if (scene == 1) {
+//			musicSelected.SetActive (true); 
+//			gameSummarySelected.SetActive (false); 
+//			startSelected.SetActive (false);
+//		} else if (scene == 2) {
+//			gameSummarySelected.SetActive (true); 
+//			musicSelected.SetActive (false); 
+//			startSelected.SetActive (false);
+//		}
+//		else {
+//			startSelected.SetActive (true);
+//			gameSummarySelected.SetActive (false); 
+//			musicSelected.SetActive (false); 
+//		}
+//
+//		Application.LoadLevel(scene);
+//	}
+//
 	void Start () {
 		
 	}
