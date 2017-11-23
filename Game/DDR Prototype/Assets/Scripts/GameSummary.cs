053972f123a2f044ad2162a0685293be92a8dc7d
Bimesh De Silva <b2desilv@edu.uwaterloo.ca>
2017-11-23 11:03:13 -0500

    Updated game summary documentation

--- Game/DDR Prototype/Assets/Scripts/GameSummary.cs
+++ Game/DDR Prototype/Assets/Scripts/GameSummary.cs
@@ -13,12 +13,18 @@ public class GameSummary : MonoBehaviour {
         UpdateScore();
     }
 
+    /*
+     * Called to update displayed game statistics in game summary
+     */
     private bool UpdateScore(){
         HitsText.text = HITS_TAG + GameNavigation.hits;
         MissesText.text = MISSES_TAG + GameNavigation.misses;
         return true;
     }
 
+    /**
+     * Called by play again button on click
+     */
     public void playAgain(){
         Debug.Log("Play again called");
         game.restartGame();
