4cbb77aff131d6810b24f432771be578f7a3402b
Rosie Bahrani <mahshidbahrani@v1020-wn-140-31.campus-dynamic.uwaterloo.ca>
2017-11-22 22:23:02 -0500

    added

---
+++ Game/DDR Prototype/Assets/SongSelectManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SongSelect : MonoBehaviour {
+
+	public void SelectTheSong (int songSelected){
+		SongSelected (songSelected); 
+	}
+
+
+}
